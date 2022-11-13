f6039c9768ffcad4440f9818cc338828926a0b20
Kirill Mironov <kirill@gmail.com>
2022-11-13 08:40:43 +0300

    ggg

--- script.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById('form');
-function retrieveFormValue(event) {
-    event.preventDefault();
-    const name = form.querySelector('[name="name"]');
-    const values = name.value;
-    console.log(values)
-}
-form.addEventListener('submit', retrieveFormValue);
